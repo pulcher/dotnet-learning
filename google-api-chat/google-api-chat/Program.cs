78d38d8aa2c4f523f6be7e5ce0fbe84a529bf062
Harold Pulcher <pulcher@killercomputing.com>
2024-08-18 17:19:25 -0500

    was playing with the .NET google ai sdk

---
+++ google-api-chat/google-api-chat/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace google_api_chat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
